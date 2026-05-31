f0aaf1a28aeb33da449272ade822e0f00cc81650
Ernest Gaisie <ernestyawgaisie@gmail.com>
2026-05-31 18:02:39 -0400

    Dealing with unexisting resources

--- obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
+++ obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2172402e496efa036bd33b508443a0ab580e6c97")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+483daa05f3cec5c5cc505aec35e9f5e1c27738d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
