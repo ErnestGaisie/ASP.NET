14823f12a61c3e0f14b4c933e1a7bfd241cc3453
Ernest Gaisie <ernestyawgaisie@gmail.com>
2026-05-31 19:36:37 -0400

    Refactored the endpoints

--- obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
+++ obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+483daa05f3cec5c5cc505aec35e9f5e1c27738d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0aaf1a28aeb33da449272ade822e0f00cc81650")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
