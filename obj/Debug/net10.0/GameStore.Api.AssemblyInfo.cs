0e9befe4dcfe67dad4c2f6f9ae5dd8ad83f8c44f
Ernest Gaisie <ernestyawgaisie@gmail.com>
2026-06-03 12:52:33 -0400

    Connectiong database to endpoint

--- obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
+++ obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0606bdebc0220ba33859ea2206c35df4a7bb4ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8be388baa081001a474388ff42c5f74ba9d6c465")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
